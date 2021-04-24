51cc8ce6897f2fe2514d0be4976f657a16226e6b
Iago Rodrigues <yagomello16@gmail.com>
2021-04-24 00:23:05 -0300

    feat: add UserDTO

---
+++ src/main/java/dev/iagorodrigues/userapi/dto/UserDTO.java
@@ -0,0 +1,62 @@
+package dev.iagorodrigues.userapi.dto;
+
+import java.time.Instant;
+
+public class UserDTO {
+
+    private String name;
+    private String socialSecurityNumber;
+    private String address;
+    private String email;
+    private String phone;
+    private Instant registerDate;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSocialSecurityNumber() {
+        return socialSecurityNumber;
+    }
+
+    public void setSocialSecurityNumber(String socialSecurityNumber) {
+        this.socialSecurityNumber = socialSecurityNumber;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public Instant getRegisterDate() {
+        return registerDate;
+    }
+
+    public void setRegisterDate(Instant registerDate) {
+        this.registerDate = registerDate;
+    }
+
+}

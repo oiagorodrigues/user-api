5996c4433784b1764d140be0bc87cb3c54ea57d5
Iago Rodrigues <yagomello16@gmail.com>
2021-04-23 21:14:58 -0300

    initial commit

---
+++ src/main/java/dev/iagorodrigues/userapi/controller/UserController.java
@@ -0,0 +1,12 @@
+package dev.iagorodrigues.userapi.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+    @GetMapping("/")
+    public String getMessage() {
+        return "Spring is working";
+    }
+}
